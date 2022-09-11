328e529e7593f7931dd018a24e0198487a78d65c
artifact911 <artifact911@gmail.com>
2022-09-11 21:48:49 +0300

    lesson28 done

--- src/http/servlet/FirstServlet.java
+++ src/http/servlet/FirstServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -19,7 +21,18 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+      // можем получить хедер по ключу
+       req.getHeader("user-agent");
+       // или получить все
+        var headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            var header = headerNames.nextElement();
+            System.out.println(header);
+        }
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+        // кодировку лучше пердавать явно так либо:
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         try (var writer = resp.getWriter()) {
             writer.write("<h1>Hello from first servlet</h2>");
         }
