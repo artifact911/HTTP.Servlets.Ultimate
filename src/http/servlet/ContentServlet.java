59b0d1434eea1d388e4f984501cdb3e492b6e688
artifact911 <artifact911@gmail.com>
2022-09-14 15:28:36 +0300

    lesson42 EL-ExpressionLanguage done

--- src/http/servlet/ContentServlet.java
+++ src/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package http.servlet;
 
+import http.dto.FlightDto;
+import http.service.FlightService;
 import http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // установим артибуты чтобы достать из при помощи EL (expression language) - lesson42
+        List<FlightDto> flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+
+        // тут установили в sessionScope
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -13,7 +13,19 @@
 <body>
     <%@include file="header.jsp"%>
     <div>
-        Content. Русский
+
+<%--        http://localhost:8081/content?id=5&test=hello--%>
+        <span>Content. Русский</span>
+
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights.get(0).id}</p>
+        <p>Id 2: ${requestScope.flights[1].id}</p>
+        <p>Map id 2: ${sessionScope.flightsMap[1]}</p>
+        <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+        <p>Empty list: ${not empty requestScope.flights}</p>
     </div>
     <%@include file="footer.jsp"%>
 </body>
