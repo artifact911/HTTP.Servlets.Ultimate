8b7161995e5149281adf3d43593e6adbdeaa3f11
artifact911 <artifact911@gmail.com>
2022-09-15 12:35:44 +0300

    lesson44 JSTL practice done

--- src/http/servlet/FlightServlet.java
+++ src/http/servlet/FlightServlet.java
@@ -1,6 +1,7 @@
 package http.servlet;
 
 import http.service.FlightService;
+import http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,21 +18,29 @@ public class FlightServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
-        var printWriter = resp.getWriter();
-            printWriter.write("<h1>Список перелетов:</h1>");
-            printWriter.write("<ul>");
+        req.setAttribute("flights", flightService.findAll());
 
-            flightService.findAll().forEach(flightDto -> {
-                printWriter.write("""
-                        <li>
-                            <a href="tickets?flightId=%d">%s</a>
-                        </li>
-                        """
-                        .formatted(flightDto.getId(), flightDto.getDescription()));
-            });
-            printWriter.write("</ul>");
+        // это из директивы page теперь берется
+//        resp.setContentType("text/html");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        req.getRequestDispatcher(JspHelper.getPath("flights"))
+                .forward(req, resp);
+
+        // скопировали все в flights.jsp
+//        var printWriter = resp.getWriter();
+//        printWriter.write("<h1>Список перелетов:</h1>");
+//        printWriter.write("<ul>");
+//
+//        flightService.findAll().forEach(flightDto -> {
+//            printWriter.write("""
+//                    <li>
+//                        <a href="tickets?flightId=%d">%s</a>
+//                    </li>
+//                    """
+//                    .formatted(flightDto.getId(), flightDto.getDescription()));
+//        });
+//        printWriter.write("</ul>");
     }
 }
--- web/WEB-INF/jsp/tickets.jsp
+++ web/WEB-INF/jsp/tickets.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 
@@ -7,12 +7,14 @@
     <title>Title</title>
 </head>
 <body>
-
-    <h1>Купленные билеты</h1>
+    <c:if test="${not empty requestScope.tickets}">
+        <h1>Купленные билеты</h1>
         <ul>
             <c:forEach var="ticket" items="${requestScope.tickets}">
-                <li>${ticket.seatNo}</li>
+                <li>${fn:toLowerCase(ticket.seatNo)}</li>
             </c:forEach>
         </ul>
+    </c:if>
+
 </body>
 </html>
