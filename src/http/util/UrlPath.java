702cf0b3949055937d748d9dbd9ca043a2e48dee
artifact911 <artifact911@gmail.com>
2022-09-22 17:16:19 +0300

    lesson55 JSP Locale done

--- src/http/util/UrlPath.java
+++ src/http/util/UrlPath.java
@@ -6,6 +6,8 @@ import lombok.experimental.UtilityClass;
 public class UrlPath {
 
     public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
     public static final String REGISTRATION = "/registration";
     public static final String IMAGES = "/images";
+    public static final String LOCALE = "/locale";
 }
